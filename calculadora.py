d1a385c15b58be21a787ab9e85aca1dd80bc0cdb
ISAC MOTTA <103297099+isacmotta@users.noreply.github.com>
2023-06-13 18:39:11 -0300

    editado

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,50 @@
+#inciando 
+#importações de bibliotecas
+import numpy
+from numpy import*
+print('Calculador físico:')
+print('CRIADO POR ISAC MOTTA')
+print('''
+[1]Velocidade
+[2]Distancia
+[3]Tempo
+[4]Calculador Atomico
+[5]
+[6]
 
+Use com
+''')
+sel=int(input('selecione uma opção: '))
+#
+#Calculos envolvendo velocidade
+if sel==1:
+    print('CÁLCULOS ENVOLVENDO VELOCIDADE')
+    print('''
+    [1]MRUV
+    [2]Equação de Torriceli
+    [3]
+    ''')
+    sel1=int(input(' '))
+
+#VELOCIDADE_MRUV
+    if sel1==1:
+       print('MRUV')
+       print('')
+#VELOCIDADE Torriceli
+    elif sel1==2:
+       print('')
+#calculos envolvendo Distancias
+elif sel==1:
+   print('Calculos envolvendo distancia')
+   print('''
+   [1]Ano-Luz
+   [2]Converso
+   ''')
+#tempo
+if sel==3:
+   print('tempo')
+   print('''
+   [1]Numero Atomico 
+   [2]
+   [3]
+   ''')
